8f6eeea6e79eec95614d1cca68a938ff317ab2b7
MacadamiaPH <at_macph@yahoo.com>
2014-12-06 15:02:13 +0800

    Updated DB content

--- sjw-app/src/main/java/com/sjw/app/entity/Event.java
+++ sjw-app/src/main/java/com/sjw/app/entity/Event.java
@@ -1,10 +1,32 @@
 package com.sjw.app.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import com.baldy.commons.models.BaseNamedEntity;
+
+import constants.EventType;
 
 @Entity(name = "EVENT")
 public class Event extends BaseNamedEntity {
 
+	@Column
+	@Enumerated(EnumType.STRING)
+	private EventType etype = null;
+	
+	@ManyToOne
+	@JoinColumn(name="eventowner")
+	private Organization org = null;
+	
+	@Column
+	private float latitude = 0.0f;
+	
+	@Column
+	private float longtitude = 0.0f;
+	
+	
 }
---
+++ sjw-app/src/main/java/constants/EventType.java
@@ -0,0 +1,5 @@
+package constants;
+
+public enum EventType {
+	EDU, CULT, OUTREACH, DISASTER;
+}
